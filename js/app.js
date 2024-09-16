18fcbab3ce0ba4f4359dc4225be53faf0af6af06
Annbel796 <annbel796@ihmeducation.se>
2024-09-16 10:26:06 +0200

    Rafactor

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@ let numberOfBottles = 10;
 const line1 = "green bottles hanging on the wall";
 const line2 = "and if one green bottle, should accidentally fall, there will be";
 
+console.log (numberOfBottles + line1 + numberOfBottles + line1);
+console.log (line2 + --numberOfBottles + line1);
+
+
 console.log(numberOfBottles);
 console.log(line1);
 console.log(numberOfBottles);
@@ -18,5 +22,6 @@ console.log(--numberOfBottles);
 console.log(--numberOfBottles);
 console.log(--numberOfBottles);
 console.log(line1);
+
 
 
